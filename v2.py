6a685e4369ab20028e66da4fd2f232cbd3d2cbab
steiyve <94018684+steiyve@users.noreply.github.com>
2023-08-31 10:38:47 -0400

    biutifeied v2.py

--- v2.py
+++ v2.py
@@ -1,78 +1,97 @@
+# -*- coding: utf-8 -*-
 import random
 
-#variable de programe
+# variable de programe
+
 quit = False
 score = 0
 best_score = 0
 run = True
 reponse = 0
 
-#fonction qui pose la question
+
+# fonction qui pose la question
+
 def question():
     global reponse
 
-    #poser la question 
-    essai = input("entrez un nombre entre 0 et 1000: ")
+    # poser la question
 
-    #boucle qui pose la question si repones nes pas un chiffre valide
-    while (not essai.isdigit()):
-        essai = input("un chiffre valide: ")
+    essai = input('entrez un nombre entre 0 et 1000: ')
 
-    #transformer la reponse en chiffre valide
+    # boucle qui pose la question si repones nes pas un chiffre valide
+
+    while not essai.isdigit():
+        essai = input('un chiffre valide: ')
+
+    # transformer la reponse en chiffre valide
+
     reponse = int(essai)
 
-#boucle principale
+
+# boucle principale
+
 while quit != True:
-    
-    #varialbe de la boucle
+
+    # varialbe de la boucle
+
     chiffre = random.randint(0, 1000)
     repete = 0
 
-    #loader le best_score du fichier
+    # loader le best_score du fichier
+
     with open('best.txt', 'r') as reader:
         best_score = int(reader.read())
-        print(best_score)
+        print best_score
 
-    #boucle de jeu
+    # boucle de jeu
+
     while run:
 
-        #variable de boucle
+        # variable de boucle
+
         chiffre = random.randint(0, 1000)
         repete = 0
         question()
-        
-        #boucle qui permet d avoir 10 essai
-        while repete <= 10:
-            print(repete)
-            # verification pour voir qui a gagne
-            if (reponse == chiffre):
-                print("bravo tu a gagne un point")
-                score += 10 - repete
-                print(score)
-                break
 
+        # boucle qui permet d avoir 10 essai
+
+        while repete <= 10:
+            print repete
+
+            # verification pour voir qui a gagne
+
+            if reponse == chiffre:
+                print 'bravo tu a gagne un point'
+                score += 10 - repete
+                print score
+                break
+            else:
 
             # mauvaise reponse
-            else:
                 # si le nombre choisi est plus petit
-                if (reponse < chiffre):
-                    print("choississez un nombre plus grand")
+
+                if reponse < chiffre:
+                    print 'choississez un nombre plus grand'
 
                 # si le nombre choisi est plus grand
-                if (reponse > chiffre):
-                    print("choississez un nombre plus petit")
-            
+
+                if reponse > chiffre:
+                    print 'choississez un nombre plus petit'
+
             question()
             repete += 1
 
     if score == best_score:
         best_score = score
 
-    #enregistre le best_score
+    # enregistre le best_score
+
     with open('best.txt', 'w') as writer:
         writer.write(best_score)
 
-    #recommencer la partie
-    reponse = input("voulez vous rejouer (oui ou non):")
-    if reponse == "non":
+    # recommencer la partie
+
+    reponse = input('voulez vous rejouer (oui ou non):')
+    if reponse == 'non':
         quit == True
